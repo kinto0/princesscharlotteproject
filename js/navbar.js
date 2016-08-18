5e7a54185ec55a8f7248ee434c6833be5be94997
kyleai <kyleatyahoo@gmail.com>
2016-08-18 15:31:08 -0400

    navbar animations & opacity

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,11 @@
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 5) {
-    $('nav').css({"padding": "0px"});
+  if ($(document).scrollTop() > 550) {
+    $('nav').css({"padding": "0px", "background-color": "rgba(34, 34, 34, .7)", "border": "0px"});
   } else {
-    $('nav').css({"padding": "10px"});
+    $('nav').css({"padding": "10px", "background-color": "rgba(34, 34, 34, 1"});
   }
+});
+
+$('nav').hover(function(){
+	    $('nav').css({"padding": "10px", "background-color": "rgba(34, 34, 34, 1"});
 });
